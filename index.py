d6ec92f154ae7579576f70513452ccad0aa4b2c1
Lam Nguyen <nblam1994@gmail.com>
2021-10-09 15:00:39 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+APP_VERSION = os.environ.get("APP_VERSION", default="None")
+APP_HOST = os.environ.get("APP_HOST", default="None")
+
+@app.route("/")
+def hello():
+    return f"http://{APP_HOST}:5000 -> Hello Python - {APP_VERSION} - {APP_HOST}"
+  
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
