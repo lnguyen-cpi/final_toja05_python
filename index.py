582da1789603c5cb62ec10045e2ab7cd9e3a934b
Lam Nguyen <nblam1994@gmail.com>
2021-10-11 22:23:42 -0700

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ APP_HOST = os.environ.get("APP_HOST", default="None")
 
 @app.route("/")
 def hello():
-    return f"http://{APP_HOST}:5000 -> Hello Python - {APP_VERSION} - {APP_HOST}"
+    return f"http://{APP_HOST}:5000 -> Hello Python - {APP_VERSION} - {APP_HOST}, build python"
   
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
